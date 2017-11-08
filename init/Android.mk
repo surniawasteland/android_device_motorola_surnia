6751749f7ec0005a8efc107d8a2d0c420339f0bc
ElDainosor <eldainosor@gmail.com>
2017-11-08 09:52:59 -0300

    Fix #01 | my bad
    cloud9 is bad

--- init/Android.mk
+++ init/Android.mk
@@ -5,13 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-<<<<<<< HEAD
 LOCAL_SRC_FILES := init_surnia.cpp
 LOCAL_MODULE := libinit_surnia
-=======
-LOCAL_SRC_FILES := init_osprey.cpp
-LOCAL_MODULE := libinit_osprey
 LOCAL_STATIC_LIBRARIES := libbase
->>>>>>> 08676d1... osprey: Update for O
+
 
 include $(BUILD_STATIC_LIBRARY)
