2adc5fdf6c6ae32ac1528d094f68d7a95489af04
ElDainosor <eldainosor@gmail.com>
2018-07-09 20:17:33 +0000

    surnia: Let's go thrifting
    This adds a go config file on our device tree to avoid myself picking my commits to tweak it up on and on again.
    So, what's different you may ask?
    
    Theres a removal of ro.config.low_ram=true because people complains you about features like pip and notification access removed so it's a bad build because of that. Also tweaked the low ram lmk values to have a mid-normal mid-go value because there's people who treats a intel atom as it was a i5 and pretends to do high end gaming on a shitty device.
    That's all

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
+# Android Go settings
+$(call inherit-product, device/motorola/surnia/extra/go_defaults_common.mk)
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
